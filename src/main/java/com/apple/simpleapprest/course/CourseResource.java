0fa5c11f72ef4caaba70ff6805aa43bab74c1f73
Srinivas <srinivas.bathuru@gmail.com>
2020-04-29 07:35:49 +0530

    updated

--- src/main/java/com/apple/simpleapprest/course/CourseResource.java
+++ src/main/java/com/apple/simpleapprest/course/CourseResource.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-@CrossOrigin(origins = { "http://simpleappui.com" })
+@CrossOrigin(origins = { "http://simpleappui.com:80", "http://simpleappui.com:8080" })
 @RestController
 public class CourseResource {
 
