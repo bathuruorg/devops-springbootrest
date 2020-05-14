fe40e0180a9a0cc98f5bb5c4287c5b403abe4b97
[bathuru] <[bathuru.devops@gmail.com]>
2020-05-14 23:20:52 +0530

    updated

--- src/main/java/com/apple/springbootrest/util/ApiTrackingUtility.java
+++ src/main/java/com/apple/springbootrest/util/ApiTrackingUtility.java
@@ -3,6 +3,8 @@ package com.apple.springbootrest.util;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.context.annotation.Configuration;
@@ -13,11 +15,25 @@ public class ApiTrackingUtility {
 
 	private Logger logger = LogManager.getLogger(this.getClass());
 
-    @Before("execution(* com.apple.springbootrest.controller.EmployeeController.*.*(..))")
-    public void before(JoinPoint joinPoint) {
+    @Before("execution(* com.apple.springbootrest.controller.EmployeeController.*(..)))")
+    public void beforeAdvise(JoinPoint joinPoint) {
         //Advice
-        logger.info(" Check for user access ");
+        logger.info(" Before Advise ");
         logger.info(" Allowed execution for {}", joinPoint);
     }
 
+    @After("execution(* com.apple.springbootrest.controller.EmployeeController.*(..)))")
+    public void afterAdvise(JoinPoint joinPoint) {
+        //Advice
+        logger.info(" After Advise ");
+        logger.info(" Allowed execution for {}", joinPoint);
+    }
+
+    @Around("execution(* com.apple.springbootrest.controller.EmployeeController.*(..)))")
+    public void aroundAdvise(JoinPoint joinPoint) {
+        //Advice
+        logger.info(" Around Advise ");
+        logger.info(" Allowed execution for {}", joinPoint);
+    }
+    
 }
